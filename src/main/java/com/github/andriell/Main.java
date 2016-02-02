b3efb73217aa700e137dc6073573a5f7ceec59a2
andriell <root.andriell@gmail.com>
2016-02-03 01:32:11 +0300

    Start

--- src/main/java/com/github/andriell/Main.java
+++ src/main/java/com/github/andriell/Main.java
@@ -1,11 +1,7 @@
 package com.github.andriell;
 
-import org.joda.time.LocalTime;
-
 public class Main {
-
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
+        System.out.println("The current local time is: ");
     }
 }
