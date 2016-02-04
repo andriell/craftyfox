a20054e6a5f0ef743d4208f1348ad2fa039c9bd3
andriell <root.andriell@gmail.com>
2016-02-04 21:13:22 +0300

    Start

--- src/main/java/com/github/andriell/Main.java
+++ src/main/java/com/github/andriell/Main.java
@@ -5,10 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
         // Без этого событие destroy для бинов не будет вызвано
         applicationContext.registerShutdownHook();
-        Processor processor = applicationContext.getBean("renderer", Processor.class);
+        Processor processor = applicationContext.getBean("processor", Processor.class);
 
     }
 }
