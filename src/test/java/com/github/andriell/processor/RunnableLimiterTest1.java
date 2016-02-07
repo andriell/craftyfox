27cc2cbef8d8e4214f66aa2f3422aaf76dabffed
andriell <root.andriell@gmail.com>
2016-02-07 12:57:19 +0300

    start

--- src/test/java/com/github/andriell/processor/RunnableLimiterTest1.java
+++ src/test/java/com/github/andriell/processor/RunnableLimiterTest1.java
@@ -13,4 +13,49 @@ public class RunnableLimiterTest1 {
         limiter.start(new TestProcess(new TestData()));
         System.out.println(3);
     }
+
+    static class TestData implements DataInterface {
+        private static int count = 0;
+        private String number;
+        public TestData() {
+            number = Integer.toString(count++);
+        }
+
+        @Override
+        public String toString() {
+            return number;
+        }
+    }
+
+    static class TestProcess extends ProcessAbstract {
+        private static int count = 0;
+        private String name;
+
+        public TestProcess() {
+            this(null);
+        }
+
+        public TestProcess(TestData data) {
+            setData(data);
+            name = Integer.toString(count++);
+        }
+
+        public void run() {
+            TestData data = (TestData) getData();
+            for (int i = 0; i < 10; i++) {
+                System.out.println("process " + this + " task " + data + " " + i);
+                RunnableLimiter.sleep(100);
+            }
+        }
+
+        public String getTaskType() {
+            return TestData.class.toString();
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
 }
