97e736f52d69712dec0f9c90640cb4e949393835
andriell <root.andriell@gmail.com>
2016-02-07 12:21:45 +0300

    start

--- src/test/java/com/github/andriell/collection/LinkedListTest.java
+++ src/test/java/com/github/andriell/collection/LinkedListTest.java
@@ -7,12 +7,24 @@ public class LinkedListTest {
     public static void main(String[] args) {
         LinkedSet<Integer> integers = new LinkedSet<>();
         integers.addEnd(1);
+        integers.addEnd(1);
+        integers.addEnd(2);
         integers.addEnd(2);
         integers.addEnd(3);
+        integers.addEnd(3);
+        integers.addEnd(4);
         integers.addEnd(4);
         integers.addEnd(5);
         integers.addEnd(5);
+        integers.addEnd(5);
+        integers.addFirst(0);
+        integers.addFirst(0);
         integers.addFirst(0);
         System.out.println(integers);
+        if ("[0, 1, 2, 3, 4, 5]".equals(integers.toString())) {
+            System.out.println("Ok");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
