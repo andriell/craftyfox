d496dbf5d5058054524538296a30f36689ca4100
andriell <root.andriell@gmail.com>
2016-02-13 09:39:01 +0300

    start

--- src/test/java/com/github/andriell/Test.java
+++ src/test/java/com/github/andriell/Test.java
@@ -7,13 +7,13 @@ public class Test {
     public static void main(String[] args) {
         Object o = new Object();
         int hash = o.hashCode();
-        System.out.println(o.hashCode());
-        System.out.println(Integer.toBinaryString(hash));
-        System.out.println(Integer.toOctalString(hash));
+        System.out.println("Dec: " + o.hashCode());
+        System.out.println("Bin: " + Integer.toBinaryString(hash));
+        System.out.println("Oct: " + Integer.toOctalString(hash));
 
-        int i = hash >> 2;
-        System.out.println(Integer.toBinaryString(hash));
-        System.out.println(Integer.toBinaryString(i));
-
+        while(hash > 0) {
+            System.out.print(hash & 7);
+            hash = hash >> 3;
+        }
     }
 }
