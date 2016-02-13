6584943f4c72725dfab8ac7b7420a166c07cffca
andriell <root.andriell@gmail.com>
2016-02-13 10:20:39 +0300

    start

--- src/main/java/com/github/andriell/collection/HashThree.java
+++ src/main/java/com/github/andriell/collection/HashThree.java
@@ -4,18 +4,16 @@ package com.github.andriell.collection;
  * Created by Vika on 07.02.2016
  */
 public class HashThree<T> {
-    private HashThree[] three = new HashThree[8];
+    private HashThree[] three;
     private T value;
 
-    public void add(T t) {
-        int hash = t.hashCode();
-        HashThree<T>[] position = three;
-        while (hash > 0) {
+    public void add(T t, HashThree[] position, int hash) {
+        if (hash > 0) {
+            three = new HashThree[8];
             int i = hash % 8;
             if (position[i] == null) {
                 position[i].three = new HashThree[8];
             }
-            hash -= i;
         }
     }
 
--- src/test/java/com/github/andriell/Test.java
+++ src/test/java/com/github/andriell/Test.java
@@ -6,15 +6,15 @@ package com.github.andriell;
 public class Test {
     public static void main(String[] args) {
         Object o = new Object();
-        int hash = o.hashCode();
+        int hash = -100500;
         System.out.println("Dec: " + o.hashCode());
         System.out.println("Bin: " + Integer.toBinaryString(hash));
         System.out.println("Oct: " + Integer.toOctalString(hash));
         System.out.println("Oct revers: " + new StringBuilder(Integer.toOctalString(hash)).reverse());
 
-        while(hash > 0) {
+        while(hash != 0) {
             System.out.print(hash & 7);
-            hash = hash >> 3;
+            hash = hash >>> 3;
         }
     }
 }
