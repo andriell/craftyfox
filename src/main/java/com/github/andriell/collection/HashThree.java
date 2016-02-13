36573eb5dabe47ba5647a2e53f01f773c882def2
andriell <root.andriell@gmail.com>
2016-02-13 10:52:51 +0300

    start

--- src/main/java/com/github/andriell/collection/HashThree.java
+++ src/main/java/com/github/andriell/collection/HashThree.java
@@ -28,6 +28,6 @@ public class HashThree<T> {
 
     @Override
     public String toString() {
-        return Arrays.deepToString(three);
+        return Arrays.deepToString(three) + " = " + value;
     }
 }
