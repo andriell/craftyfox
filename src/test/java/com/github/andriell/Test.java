89127435059a87b8c4b8508839d0c75689b82e7e
Andriell <root.andriell@gmail.com>
2016-02-14 20:05:05 +0300

    start

--- src/test/java/com/github/andriell/Test.java
+++ src/test/java/com/github/andriell/Test.java
@@ -6,18 +6,25 @@ import com.github.andriell.collection.HashThree;
  * Created by Vika on 07.02.2016
  */
 public class Test {
+    private static final char[] alphabet = {
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
     public static void main(String[] args) {
         Object o = new Object();
         int hash = o.hashCode();
         System.out.println("Dec: " + o.hashCode());
-        System.out.println("Bin: " + Integer.toBinaryString(hash));
         System.out.println("Hex: " + Integer.toHexString(hash));
         System.out.println("Hex revers: " + new StringBuilder(Integer.toHexString(hash)).reverse());
+        System.out.println();
 
         for(byte b = 0; b < 8; b++) {
-            System.out.print(hash & 7);
-            hash = hash >>> 3;
+            int index = hash & 0xF;
+            System.out.print(alphabet[index]);
+            hash = hash >>> 4;
         }
+        System.out.println();
         System.out.println();
 
         HashThree<Object> three = new HashThree<Object>();
@@ -25,8 +32,4 @@ public class Test {
         System.out.println(three);
     }
 
-    public class Entity {
-        public Entity[] three = new Entity[10];
-        private int value;
-    }
 }
