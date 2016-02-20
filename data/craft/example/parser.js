3bae4065f30626b0a420c575e9ece3982c6ad67b
andriell <root.andriell@gmail.com>
2016-02-20 17:16:52 +0300

    Start

--- data/craft/example/parser.js
+++ data/craft/example/parser.js
@@ -1,6 +1,16 @@
 /**
+ * Описание объекта org.jsoup.nodes.Document находится по адресу http://jsoup.org/cookbook/extracting-data/selector-syntax
  * @param  document - org.jsoup.nodes.Document
  */
 function parse(document) {
     print(document.getClass().getName());
+    var links = document.select("a[href]");
+    print(links.getClass().getName());
+    var iterator = links.iterator();
+    print(iterator.getClass().getName());
+    while(iterator.hasNext()) {
+        var element = iterator.next();
+        print(element.getClass().getName());
+        print(element.attr("href"));
+    }
 }
