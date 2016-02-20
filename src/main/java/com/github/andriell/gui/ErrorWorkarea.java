6b756796b8d7f8fd15b4c98ad2e5b05f7408857a
andriell <root.andriell@gmail.com>
2016-02-20 10:31:56 +0300

    Start

---
+++ src/main/java/com/github/andriell/gui/ErrorWorkarea.java
@@ -0,0 +1,14 @@
+package com.github.andriell.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Андрей on 20.02.2016.
+ */
+public class ErrorWorkarea {
+    private JTextArea errorTextArea;
+    private JPanel panel1;
+    private JTextField sizeTextField;
+    private JButton refreshButton;
+    private JCheckBox autoRefreshCheckBox;
+}
