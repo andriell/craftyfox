1239f3d39600de95effd008dfd9c61b94904b10e
andriell <root.andriell@gmail.com>
2016-02-20 12:17:27 +0300

    Start

---
+++ src/main/java/com/github/andriell/gui/ProcessWorkArea.java
@@ -0,0 +1,16 @@
+package com.github.andriell.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Андрей on 20.02.2016.
+ */
+public class ProcessWorkArea {
+    private JPanel panel1;
+    private JLabel processCountLabel;
+    private JLabel p;
+
+    public ProcessWorkArea() {
+
+    }
+}
