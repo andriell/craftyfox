f947a9283373b1fc8ce4e2f405e87a19d4b63202
rybalko_am <a.rybalko@1pkk.ru>
2016-04-07 16:18:20 +0300

    start

--- src/main/java/com/github/andriell/Main.java
+++ src/main/java/com/github/andriell/Main.java
@@ -4,8 +4,44 @@ import com.github.andriell.gui.MainFrame;
 import com.github.andriell.processor.Manager;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Font f = new Font("Segoe UI", Font.PLAIN, 14);
+        UIManager.put("Button.font", f);
+        UIManager.put("ToggleButton.font", f);
+        UIManager.put("RadioButton.font", f);
+        UIManager.put("CheckBox.font", f);
+        UIManager.put("ColorChooser.font", f);
+        UIManager.put("ComboBox.font", f);
+        UIManager.put("Label.font", f);
+        UIManager.put("List.font", f);
+        UIManager.put("MenuBar.font", f);
+        UIManager.put("MenuItem.font", f);
+        UIManager.put("RadioButtonMenuItem.font", f);
+        UIManager.put("CheckBoxMenuItem.font", f);
+        UIManager.put("Menu.font", f);
+        UIManager.put("PopupMenu.font", f);
+        UIManager.put("OptionPane.font", f);
+        UIManager.put("Panel.font", f);
+        UIManager.put("ProgressBar.font", f);
+        UIManager.put("ScrollPane.font", f);
+        UIManager.put("Viewport.font", f);
+        UIManager.put("TabbedPane.font", f);
+        UIManager.put("Table.font", f);
+        UIManager.put("TableHeader.font", f);
+        UIManager.put("TextField.font", f);
+        UIManager.put("PasswordField.font", f);
+        UIManager.put("TextArea.font", f);
+        UIManager.put("TextPane.font", f);
+        UIManager.put("EditorPane.font", f);
+        UIManager.put("TitledBorder.font", f);
+        UIManager.put("ToolBar.font", f);
+        UIManager.put("ToolTip.font", f);
+        UIManager.put("Tree.font", f);
+
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
         // Без этого событие destroy для бинов не будет вызвано
         applicationContext.registerShutdownHook();
