a46acc1419295292200b79cf6a8e7a42440d84a2
andriell <root.andriell@gmail.com>
2016-04-09 08:58:16 +0300

    start

--- data/craft/example/process.js
+++ data/craft/example/process.js
@@ -2,7 +2,7 @@
  * Описание объекта org.jsoup.nodes.Document находится по адресу http://jsoup.org/cookbook/extracting-data/selector-syntax
  * @param  document - org.jsoup.nodes.Document
  */
-
+console.info(task.add("process-js", {}));
 $.addProcess("example", function(document) {
 	console.info(document.getClass().getName());
 	var links = document.select("a.link");
