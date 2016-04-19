a129c3a7a35f0b0f9af6b60eec060aa6e573b7c9
rybalko_am <a.rybalko@1pkk.ru>
2016-04-19 18:34:20 +0300

    start

--- src/test/java/com/github/andriell/collection/StackByteTest.java
+++ src/test/java/com/github/andriell/collection/StackByteTest.java
@@ -24,5 +24,17 @@ public class StackByteTest {
         assertEquals(stackString.toString(), "01234");
         stackString.put((byte) '5');
         assertEquals(stackString.toString(), "12345");
+        stackString.put((byte) '6');
+        assertEquals(stackString.toString(), "23456");
+        stackString.put((byte) '7');
+        assertEquals(stackString.toString(), "34567");
+        stackString.put((byte) '8');
+        assertEquals(stackString.toString(), "45678");
+        stackString.put((byte) '9');
+        assertEquals(stackString.toString(), "56789");
+        stackString.put((byte) 'a');
+        assertEquals(stackString.toString(), "6789a");
+        stackString.put((byte) 'b');
+        assertEquals(stackString.toString(), "789ab");
     }
 }
