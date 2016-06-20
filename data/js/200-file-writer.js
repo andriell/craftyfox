4a158c85a4a0ec94cb2210a92ca4faf025ea3efc
rybalko_am <a.rybalko@1pkk.ru>
2016-06-20 12:24:28 +0300

    start

--- data/js/200-file-writer.js
+++ data/js/200-file-writer.js
@@ -2,13 +2,51 @@
  * Created by Rybalko on 20.06.2016.
  */
 
-function csvWriter(fileName) {
+function FileWriter(fileName) {
     var writer = app.getBean("file-writer");
     writer.init(fileName);
     var fileWriter = {
         "writer": writer,
         "write": function (s) {
             fileWriter.writer.write(s);
+        },
+        "newLine": function () {
+            fileWriter.writer.newLine();
+        },
+        "flush": function () {
+            fileWriter.writer.flush();
+        },
+        "close": function () {
+            fileWriter.writer.flush();
+            fileWriter.writer.close();
+        }
+    };
+    return fileWriter;
+}
+
+
+function CsvWriter(fileName) {
+    var writer = app.getBean("file-writer");
+    writer.init(fileName);
+    var fileWriter = {
+        "writer": writer,
+        "write": function (list) {
+            var prefix = "";
+            if (Array.isArray(list)) {
+                for(var i = 0; i < list.length; i++) {
+                    fileWriter.writer.write(prefix);
+                    fileWriter.writer.write(list[i].replace("\t", " "));
+                    prefix = "\t";
+                }
+                fileWriter.writer.newLine();
+            } else if (typeof(list) == "object") {
+                for(var i in list) {
+                    fileWriter.writer.write(prefix);
+                    fileWriter.writer.write(list[i].replace("\t", " "));
+                    prefix = "\t";
+                }
+                fileWriter.writer.newLine();
+            }
         },
         "flush": function () {
             fileWriter.writer.flush();
--- data/projects/example/test4/process.js
+++ data/projects/example/test4/process.js
@@ -1,8 +1,8 @@
 $.addParser("example.test4", function(d) {
-	var fileWriter = csvWriter("test4.txt");
+	var fileWriter = FileWriter("test4.txt");
 	fileWriter.write(100500);
 
-	var fileWriter2 = csvWriter("test5.txt");
+	var fileWriter2 = FileWriter("test5.txt");
 	fileWriter2.write(100501);
 	
 	fileWriter.close();
