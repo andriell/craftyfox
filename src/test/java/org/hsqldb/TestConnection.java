f2f8515f922e7616fe725c936e1cb20d9b95af8e
rybalko_am <a.rybalko@1pkk.ru>
2016-06-21 12:08:38 +0300

    start

--- src/test/java/org/hsqldb/TestConnection.java
+++ src/test/java/org/hsqldb/TestConnection.java
@@ -1,9 +1,9 @@
 package org.hsqldb;
 
-import org.hsqldb.jdbc.JDBCPool;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -18,7 +18,7 @@ public class TestConnection {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
         // Без этого событие destroy для бинов не будет вызвано
         applicationContext.registerShutdownHook();
-        JDBCPool jdbcPool = applicationContext.getBean("db-connection-pool", JDBCPool.class);
+        DataSource jdbcPool = applicationContext.getBean("db-connection-pool", DataSource.class);
         Connection c1 = jdbcPool.getConnection();
         Connection c2 = jdbcPool.getConnection();
         assertEquals("Пустая строка", true, c1.equals(c2));
