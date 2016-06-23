a33091b6aff11de798d8222a7925b64414307ee3
rybalko_am <a.rybalko@1pkk.ru>
2016-06-23 17:16:06 +0300

    start

--- src/main/java/com/github/andriell/db/ProductDaoImpl.java
+++ src/main/java/com/github/andriell/db/ProductDaoImpl.java
@@ -17,11 +17,9 @@ public class ProductDaoImpl implements ProductDao {
 
     private SessionFactory sessionFactory;
 
-    public Product findByCode(String code) {
-        List<Product> users = getSessionFactory()
-                .getCurrentSession()
-                .createQuery("from Product where code=?")
-                .setParameter(0, code)
+    public Product findByCode(String code, Session session) {
+        List<Product> users = session.createQuery("from Product where code=:code")
+                .setParameter("code", code)
                 .list();
         if (users.size() > 0) {
             return users.get(0);
@@ -30,48 +28,39 @@ public class ProductDaoImpl implements ProductDao {
         }
     }
 
+    public int clearProperty(int productId, Session session) {
+        return session.createQuery("delete from ProductProperty where name<>:name AND product_id = :product_id")
+                .setParameter("name", PRICE)
+                .setParameter("product_id", productId)
+                .executeUpdate();
+    }
+
+    public List<ProductProperty> getProperty(int productId, String propertyName) {
+        return getSessionFactory()
+                .getCurrentSession()
+                .createQuery("from ProductProperty where product_id=:product_id AND name=:name")
+                .setParameter("product_id", productId)
+                .setParameter("name", propertyName)
+                .list();
+    }
+
     public boolean save(Product product) {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        Product p = findByCode(product.getCode());
+        Product p = findByCode(product.getCode(), session);
         if (p != null) {
             product.setId(p.getId());
         }
         session.save(product);
-
+        clearProperty(product.getId(), session);
         Set<ProductProperty> properties = product.getProperty();
         for (ProductProperty property: properties) {
-            if (PRICE.equals(product.getName())) {
-                session.save(property);
-            } else if (property.getIsArray() == null) {
-
-            } else {
-
-            }
+            session.save(property);
         }
-
         session.getTransaction().commit();
 
         return true;
-    }
-
-    public boolean _save(Product product) {
-        Session session = sessionFactory.openSession();
-        try {
-            session.beginTransaction();
-            session.save(product);
-            Set<ProductProperty> properties = product.getProperty();
-            for (ProductProperty property: properties) {
-                session.save(property);
-            }
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            LOG.error(this, e);
-            session.getTransaction().rollback();
-            return false;
-        }
     }
 
     public SessionFactory getSessionFactory() {
