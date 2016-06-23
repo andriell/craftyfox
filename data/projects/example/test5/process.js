4d368481e2cecdbab7cbc0f565739501a910c606
rybalko_am <a.rybalko@1pkk.ru>
2016-06-23 18:39:39 +0300

    start

--- data/projects/example/test5/process.js
+++ data/projects/example/test5/process.js
@@ -1,5 +1,5 @@
 $.addParser("example.test5", function(d) {
-	console.info("test3");
+	console.info("test5");
 	var dataHttp = $.newHttpData("GET", "http://ya.ru");
 	var dataHtml = $.newJsDataHtml("example.test5resp");
 	dataHttp.addDataListener(dataHtml);
@@ -9,9 +9,10 @@ $.addParser("example.test5", function(d) {
 
 $.addParser("example.test5resp", function(dataHtml) {
 	var product = Product("ya.ru", "test5");
+	product.setUrl("http://ya.ru/");
 	product.addProperty(ProductProperty("p1", 1));
 	product.addProperty(ProductProperty("p2", "2"));
 	product.addProperty(ProductProperty("p3", 3.5));
 	product.addProperty(ProductPrice(50.5, "RUB"));
-	product.save();
+	console.info(product.save());
 });
