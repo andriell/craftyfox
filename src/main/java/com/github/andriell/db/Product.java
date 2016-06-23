dc2fbc0df82ef88ad0a06407cc88489565d0e121
rybalko_am <a.rybalko@1pkk.ru>
2016-06-23 10:33:00 +0300

    start

--- src/main/java/com/github/andriell/db/Product.java
+++ src/main/java/com/github/andriell/db/Product.java
@@ -1,7 +1,48 @@
 package com.github.andriell.db;
 
+import java.sql.Date;
+
 /**
  * Created by Andrey on 22.06.2016
  */
 public class Product {
+    private int id;
+    private String code;
+    private String name;
+    private String url;
+    private int price;
+    private String currency;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- src/main/java/com/github/andriell/db/ProductPrice.java
+++ src/main/java/com/github/andriell/db/ProductPrice.java
@@ -1,7 +1,14 @@
 package com.github.andriell.db;
 
+import java.sql.Date;
+
 /**
  * Created by Andrey on 22.06.2016
  */
 public class ProductPrice {
+    private int productId;
+    private int price;
+    private String currency;
+    private Date date;
+
 }
