a5a1e4e7e19113ddb788b26b8fe42a0f6bb4e854
rybalko_am <a.rybalko@1pkk.ru>
2016-07-01 15:48:24 +0300

    start

--- src/main/java/com/github/andriell/gui/ProductsWorkArea.java
+++ src/main/java/com/github/andriell/gui/ProductsWorkArea.java
@@ -1,16 +1,21 @@
 package com.github.andriell.gui;
 
+import org.springframework.beans.factory.InitializingBean;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by Rybalko on 01.07.2016.
  */
-public class ProductsWorkArea implements WorkArea {
+public class ProductsWorkArea implements WorkArea, InitializingBean {
     private String name = "Продукты";
     private JPanel rootPanel;
     private JPanel paginationPanel;
     private JPanel dataPanel;
     private JButton addButton;
+    private JPanel filterPane;
 
     public String getName() {
         return name;
@@ -22,5 +27,19 @@ public class ProductsWorkArea implements WorkArea {
 
     public JPanel getRootPanel() {
         return rootPanel;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        addButton.addActionListener(new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                filterPane.add(newFilter());
+            }
+        });
+    }
+
+    protected Component newFilter() {
+        JPanel jPanel = new JPanel(new FlowLayout());
+        jPanel.add(new JLabel("100500"));
+        return jPanel;
     }
 }
