cd85d0cf3eef0e8b5c5a0f71c23b3850c6edf297
rybalko_am <a.rybalko@1pkk.ru>
2016-07-13 11:41:19 +0300

    start

--- src/main/java/com/github/andriell/db/HashDateDao.java
+++ src/main/java/com/github/andriell/db/HashDateDao.java
@@ -4,4 +4,8 @@ package com.github.andriell.db;
  * Created by Rybalko on 13.07.2016.
  */
 public interface HashDateDao {
+    boolean checkSec(String str, int sec);
+    boolean checkMinute(String str, int i);
+    boolean checkHour(String str, int i);
+    boolean checkDay(String str, int i);
 }
