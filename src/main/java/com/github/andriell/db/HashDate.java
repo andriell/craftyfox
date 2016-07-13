808e3f76b35d64209a4fd0b38f34d90e2ad1c820
rybalko_am <a.rybalko@1pkk.ru>
2016-07-13 11:36:36 +0300

    start

--- src/main/java/com/github/andriell/db/HashDate.java
+++ src/main/java/com/github/andriell/db/HashDate.java
@@ -2,13 +2,11 @@ package com.github.andriell.db;
 
 import org.springframework.util.DigestUtils;
 
-import java.util.Date;
-
 /**
  * Created by Rybalko on 13.07.2016.
  */
 public class HashDate {
-    private Date date;
+    private long date;
     private String hash;
 
     public String getHash() {
@@ -16,14 +14,18 @@ public class HashDate {
     }
 
     public void setHash(String str) {
+        hash = str;
+    }
+
+    public void setString(String str) {
         hash = DigestUtils.md5DigestAsHex(str.getBytes());
     }
 
-    public Date getDate() {
+    public long getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(long date) {
         this.date = date;
     }
 }
--- src/main/java/com/github/andriell/db/HashDateDaoImpl.java
+++ src/main/java/com/github/andriell/db/HashDateDaoImpl.java
@@ -2,16 +2,15 @@ package com.github.andriell.db;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.InitializingBean;
+import org.hibernate.Transaction;
 import org.springframework.util.DigestUtils;
 
-import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Rybalko on 13.07.2016.
  */
-public class HashDateDaoImpl implements HashDateDao, InitializingBean {
+public class HashDateDaoImpl implements HashDateDao {
     private SessionFactory sessionFactory;
 
 
@@ -33,19 +32,23 @@ public class HashDateDaoImpl implements HashDateDao, InitializingBean {
 
         if (hashDate == null) {
             Session session = sessionFactory.openSession();
-            hashDate = new HashDate();
-            hashDate.setHash(str);
-            hashDate.setDate(new Date());
-            session.save(hashDate);
+            Transaction transaction = session.beginTransaction();
+            HashDate hashDate2 = new HashDate();
+            hashDate2.setString(str);
+            hashDate2.setDate(System.currentTimeMillis());
+            session.save(hashDate2);
+            transaction.commit();
             session.close();
             return true;
         }
-        if (hashDate.getDate().getTime() + sec * 1000 > System.currentTimeMillis()) {
+        if (hashDate.getDate() + sec * 1000 > System.currentTimeMillis()) {
             return false;
         }
         Session session = sessionFactory.openSession();
-        hashDate.setDate(new Date());
+        Transaction transaction = session.beginTransaction();
+        hashDate.setDate(System.currentTimeMillis());
         session.update(hashDate);
+        transaction.commit();
         session.close();
         return true;
     }
@@ -60,10 +63,6 @@ public class HashDateDaoImpl implements HashDateDao, InitializingBean {
 
     public boolean checkDay(String str, int i) {
         return checkSec(str, i * 86400);
-    }
-
-    public void afterPropertiesSet() throws Exception {
-
     }
 
     public SessionFactory getSessionFactory() {
