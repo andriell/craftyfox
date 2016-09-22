c1deb171c555476b84525e2ad23a9ac260333231
rybalko_am <a.rybalko@1pkk.ru>
2016-09-22 17:51:02 +0300

    informer

---
+++ data/projects/example/informer/process.js
@@ -0,0 +1,9 @@
+/**
+ * Описание объекта org.jsoup.nodes.Document находится по адресу http://jsoup.org/cookbook/extracting-data/selector-syntax
+ * @param  document - org.jsoup.nodes.Document
+ */
+
+$.addParser("example.informer", function(data) {
+	//informer.beep();
+	informer.skype('test');
+});
--- src/main/java/com/github/andriell/nashorn/informer/Beep.java
+++ src/main/java/com/github/andriell/nashorn/informer/Beep.java
@@ -10,5 +10,4 @@ public class Beep {
         Toolkit.getDefaultToolkit().beep();
         return true;
     }
-
 }
