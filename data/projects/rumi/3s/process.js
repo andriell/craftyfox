905381d0392b6b0fa9d4187be4183eb8ac0763ab
rybalko_am <a.rybalko@1pkk.ru>
2016-09-22 11:51:44 +0300

    informer

--- data/projects/rumi/3s/process.js
+++ data/projects/rumi/3s/process.js
@@ -4,4 +4,12 @@ $.addParser("rumi.3s", function (data) {
     if (buttonBay.size() != 0) {
         informer.beep();
     }
+    console.sleep(10);
+
+    var url = 'http://ru-mi.com/xiaomi-redmi-3s-3gb-32gb-chernyiy';
+    var dataHttp = $.newHttpData("GET", url);
+    var dataHtml = $.newJsDataHtml("rumi.3s");
+    dataHttp.addDataListener(dataHtml);
+    console.info("Add data: " + dataHttp);
+    craftyFox.processor.add("process-http", dataHttp);
 });
--- data/projects/rumi/start/process.js
+++ data/projects/rumi/start/process.js
@@ -1,12 +1,8 @@
 $.addParser("rumi.start", function (data) {
     var url = 'http://ru-mi.com/xiaomi-redmi-3s-3gb-32gb-chernyiy';
-    while(true) {
-        console.info(url);
-        var dataHttp = $.newHttpData("GET", url);
-        var dataHtml = $.newJsDataHtml("rumi.3s");
-        dataHttp.addDataListener(dataHtml);
-        console.info("Add data: " + dataHttp);
-        craftyFox.processor.add("process-http", dataHttp);
-        console.sleep(10);
-    }
+    var dataHttp = $.newHttpData("GET", url);
+    var dataHtml = $.newJsDataHtml("rumi.3s");
+    dataHttp.addDataListener(dataHtml);
+    console.info("Add data: " + dataHttp);
+    craftyFox.processor.add("process-http", dataHttp);
 });
